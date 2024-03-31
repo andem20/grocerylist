4dfbad3dd1a3fa511e5fe425f32039f9efeab7c1
Anders Lindhardt Madsen <andem20@student.sdu.dk>
2024-03-31 09:30:23 +0200

    improve w2v loading

--- rust-backend/word2vec/src/main.rs
+++ rust-backend/word2vec/src/main.rs
@@ -1,9 +1,11 @@
+use core::num;
 use std::{
     collections::HashMap,
     fs::File,
     io::{Read, Write},
     str::Utf8Error,
     time::Instant,
+    vec,
 };
 
 // Number of words (4 bytes), Embedding length (4 bytes)
@@ -13,9 +15,10 @@ fn main() {
     let start = Instant::now();
     let embeddings_map: HashMap<String, Vec<f32>> =
         load_w2v("./resources/glove-wiki-gigaword-300.w2v");
+
     println!(
         "Loaded {} words in {:.3}s",
-        &embeddings_map.keys().len(),
+        embeddings_map.keys().len(),
         start.elapsed().as_secs_f32()
     );
 
@@ -26,65 +29,69 @@ fn main() {
     println!("Dist: {}", dist);
 }
 
+fn load_w2v(file_name: &str) -> HashMap<String, Vec<f32>> {
+    let mut file = std::fs::File::open(file_name).unwrap();
+    let mut buffer: Vec<u8> = vec![];
+    let _ = file.read_to_end(&mut buffer);
+
+    let (num_words, embedding_size, cursor) = read_headers(&buffer);
+    let (words, cursor) = read_words(&buffer, num_words, cursor);
+    let (embeddings, _) = read_embeddings(&buffer, num_words, embedding_size, cursor);
+
+    return words.into_iter().zip(embeddings).collect();
+}
+
+fn read_embeddings(
+    buffer: &[u8],
+    num_words: usize,
+    embedding_size: usize,
+    mut cursor: usize,
+) -> (Vec<Vec<f32>>, usize) {
+    let mut embeddings: Vec<Vec<f32>> = vec![];
+
+    for _ in 0..num_words {
+        let size = std::mem::size_of::<f32>();
+        let end = cursor + embedding_size as usize * size;
+        let embedding: Vec<f32> = buffer[cursor..end]
+            .chunks_exact(size)
+            .map(|chunk| f32::from_le_bytes(chunk.try_into().unwrap()))
+            .collect();
+
+        embeddings.push(embedding);
+        cursor = end;
+    }
+
+    return (embeddings, cursor);
+}
+
+fn read_words(buffer: &[u8], num_words: usize, mut cursor: usize) -> (Vec<String>, usize) {
+    let mut words = vec![];
+    for _ in 0..num_words {
+        let word_length = buffer[cursor];
+        let start = cursor + 1;
+        let end = start + word_length as usize;
+        let word = std::str::from_utf8(&buffer[start..end])
+            .map(|s| s.to_string())
+            .unwrap();
+        words.push(word);
+        cursor = end;
+    }
+
+    return (words, cursor);
+}
+
+fn read_headers(buffer: &[u8]) -> (usize, usize, usize) {
+    let num_words = u32::from_le_bytes(buffer[0..4].try_into().unwrap());
+    let embedding_size = u32::from_le_bytes(buffer[4..8].try_into().unwrap());
+    return (num_words as usize, embedding_size as usize, 8);
+}
+
 fn distance(vec_a: &Vec<f32>, vec_b: &Vec<f32>) -> f32 {
     return vec_a
         .iter()
         .zip(vec_b.iter())
         .map(|(&a_val, &b_val)| (a_val - b_val) * (a_val - b_val))
         .fold(0.0, std::ops::Add::add);
-}
-
-fn load_w2v(file_name: &str) -> HashMap<String, Vec<f32>> {
-    let file_read = std::fs::File::open(file_name).unwrap();
-
-    let (num_words, embedding_size) = read_headers(&file_read).unwrap();
-
-    let mut words = vec![];
-
-    for _i in 0..num_words {
-        let word = read_word(&file_read).unwrap();
-        words.push(word);
-    }
-
-    let mut embeddings: Vec<Vec<f32>> = vec![];
-
-    for _i in 0..num_words {
-        let embedding = read_embedding(&file_read, embedding_size as usize);
-        embeddings.push(embedding.try_into().unwrap());
-    }
-
-    return words.into_iter().zip(embeddings).collect();
-}
-
-fn read_headers(mut file: &File) -> Result<(u32, u32), std::io::Error> {
-    let mut buffer = [0; 8];
-
-    let _ = file.read_exact(&mut buffer);
-
-    let num_words = u32::from_le_bytes(buffer[0..4].try_into().unwrap());
-    let embedding_size = u32::from_le_bytes(buffer[4..].try_into().unwrap());
-
-    Ok((num_words, embedding_size))
-}
-
-fn read_word(mut file: &File) -> Result<String, Utf8Error> {
-    let mut word_length = [0; 1];
-    let _ = file.read_exact(&mut word_length);
-
-    let mut buffer = vec![0; word_length[0] as usize].into_boxed_slice();
-    let _ = file.read_exact(&mut buffer);
-
-    return std::str::from_utf8(&buffer).map(|s| s.to_string());
-}
-
-fn read_embedding(mut file: &File, embedding_length: usize) -> Vec<f32> {
-    let mut buffer = vec![0; embedding_length * 4].into_boxed_slice();
-    let _ = file.read_exact(&mut buffer);
-
-    buffer
-        .chunks(4)
-        .map(|chunk| f32::from_le_bytes(chunk.try_into().unwrap()))
-        .collect()
 }
 
 #[allow(dead_code)]
